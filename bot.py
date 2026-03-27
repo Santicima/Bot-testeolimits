582b5d89370566c96ee81a6b22d4d3a4bfd56f6a
Santicima <131073858+Santicima@users.noreply.github.com>
2026-03-27 02:54:04 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import requests
 
 API_KEY = "67acd669ed652da798ba482d69c33a95"
 
-url = f"https://api.the-odds-api.com/v4/sports/?apiKey={API_KEY}"
+url = f"https://api.the-odds-api.com/v4/sports/soccer_argentina_primera_division/odds/?apiKey={API_KEY}&regions=eu&markets=h2h"
 
 res = requests.get(url)
 
