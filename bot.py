61454721201b56e8e022955216c57e5302011c96
Santicima <131073858+Santicima@users.noreply.github.com>
2026-04-07 13:28:37 -0300

    Actualizar bot.py

--- bot.py
+++ bot.py
@@ -21,13 +21,13 @@ except Exception as e:
 print(“Error Telegram:”, e)
 
 options = Options()
-options.add_argument(”–headless=new”)
-options.add_argument(”–no-sandbox”)
-options.add_argument(”–disable-dev-shm-usage”)
-options.add_argument(”–disable-gpu”)
-options.add_argument(”–disable-software-rasterizer”)
-options.add_argument(”–window-size=1920,1080”)
-options.add_argument(”–user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36”)
+options.add_argument(”--headless=new”)
+options.add_argument(--no-sandbox”)
+options.add_argument(”--disable-dev-shm-usage”)
+options.add_argument("--disable-gpu”)
+options.add_argument(”--disable-software-rasterizer”)
+options.add_argument(”--window-size=1920,1080”)
+options.add_argument(”--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36”)
 options.binary_location = “/usr/bin/chromium”
 
 driver = webdriver.Chrome(
