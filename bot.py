884d0d542a8d062df10d4f2d9b5f9a784187a330
Santicima <131073858+Santicima@users.noreply.github.com>
2026-04-06 23:02:17 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 print("🔥 SCRIPT ARRANC1Ó")
 
 import time
-
+options.add_argument("--disable-software-rasterizer")
+options.add_argument("--remote-debugging-port=9222")
 while True:
     print("⏳ vivo...")
     time.sleep(5)
